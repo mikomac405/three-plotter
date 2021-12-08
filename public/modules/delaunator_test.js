e85317d1dc587f14ca0fb9e6c094e104d37bb6f4
NGine <56292502+mikomac405@users.noreply.github.com>
2021-12-08 18:01:26 +0100

    Delaunator works xd

--- public/modules/delaunator_test.js
+++ public/modules/delaunator_test.js
@@ -3,19 +3,28 @@ import Delaunator from 'delaunator/index';
 
 function renderSinFunFromPoint(){
     var size = { x: 5, y: 5 };
-    var pointsCount = 500;
+    var pointsCount = 3;
     var points3d = [];
-    for (let i = 0; i < pointsCount; i++) {
+    for (let i = 0; i < pointsCount; i+=0.1) {
         let x = THREE.Math.randFloatSpread(size.x);
         let z = THREE.Math.randFloatSpread(size.y);
         let y = THREE.Math.randFloatSpread(x / size.x * 5, z / size.y * 5);
-        points3d.push(new THREE.Vector3(x, y, z));
+        //points3d.push(new THREE.Vector3(x, y, z));
     }
 
+    for (let x = -3; x < pointsCount; x+=0.1){
+        for (let z = -3; z < pointsCount; z+=0.1){
+            let y = x*x+(z/2);
+            points3d.push(new THREE.Vector3(x,y,z));
+        }
+    }
+
+    let mat = new THREE.PointsMaterial({ color: 0x99ccff, size: 2 })
+    mat.side = 2
     var geom = new THREE.BufferGeometry().setFromPoints(points3d);
     var cloud = new THREE.Points(
     geom,
-    new THREE.PointsMaterial({ color: 0x99ccff, size: 2 })
+    
     );
 
     // triangulate x, z
@@ -36,6 +45,8 @@ function renderSinFunFromPoint(){
     geom, // re-use the existing geometry
     new THREE.MeshLambertMaterial({ color: "purple", wireframe: false })
     );
+    mesh.material.flatShading = false
+
     return cloud, mesh;
 }
 
